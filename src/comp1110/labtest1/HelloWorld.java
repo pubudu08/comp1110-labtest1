6930f543b59aa6b1fbc20a077d997e490aef4175
pubudu08 <pubudu08@gmail.com>
2015-08-01 14:01:36 +1000

    Setup the project

---
+++ src/comp1110/labtest1/HelloWorld.java
@@ -0,0 +1,11 @@
+package comp1110.labtest1;
+
+/**
+ * Created by Pubudu Dissanayake on 8/1/15.
+ * Projetct : comp1110-labtest1
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
